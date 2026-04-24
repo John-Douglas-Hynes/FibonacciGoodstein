7710b3a96ee0cf2123a75823815d1afe772386c1
John Hynes <j.hynes85v@gmail.com>
2026-04-24 15:01:29 -0600

    Add tree class with printing algorithms

---
+++ TreeAlgs.py
@@ -0,0 +1,21 @@
+class tree:
+    def __init__(self, val = 0, children = []):
+        self.val = val
+        self.children = children
+    
+    def pretty_print(self, node, depth_string = ''):
+        print(f"{depth_string} {node.val}")
+        for chld in node.children:
+            self.pretty_print(chld, depth_string + ' ')
+
+    def fibonacci_print(self, node, depth_string = ''):
+        if node.val == 0:
+            print(f"{depth_string} F_")
+        else:
+            print(f"{depth_string} F{{{node.val}}}")
+        for chld in node.children:
+            self.fibonacci_print(chld, depth_string + '  ')
+
+### Testing
+root = tree(0, [tree(2, []), tree(0, [tree(5, []), tree(6, [])]), tree(4, [])])
+root.fibonacci_print(root, '')
